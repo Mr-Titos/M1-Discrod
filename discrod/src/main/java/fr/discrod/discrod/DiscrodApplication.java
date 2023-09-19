7a38f774ff40348e0adbea9446791dd4baa9ec08
Mr Titos <arthur.titos2@gmail.com>
2023-09-19 14:05:39 +0200

    add java project back-end

---
+++ discrod/src/main/java/fr/discrod/discrod/DiscrodApplication.java
@@ -0,0 +1,13 @@
+package fr.discrod.discrod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscrodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscrodApplication.class, args);
+	}
+
+}
---
+++ discrod/src/test/java/fr/discrod/discrod/DiscrodApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.discrod.discrod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscrodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
